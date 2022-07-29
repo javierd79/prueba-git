77444a39c828763edac5aa44b5780571a26c56d9
Javier <javierdiazt406@gmail.com>
2022-07-28 23:26:15 -0400

    test

--- biblioteca/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import indexPage, registro, login, adminHome
-
-urlpatterns = [
-    path('', indexPage),
-    path('registro', registro),
-    path('login', login),
-    path('adminHome', adminHome)
-]
